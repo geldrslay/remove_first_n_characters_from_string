3014e678139aab64395b1268cf57cfa31670ebdb
Angela Ferrer <gelferrero01@gmail.com>
2024-01-25 01:27:14 +0800

    display message and defining original string

--- exercise_four.py
+++ exercise_four.py
@@ -1,6 +1,9 @@
 # Remove first n characters from a string
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
 
+# Display Message 
+print ('Removing certain number of characters from original string...')
+
 # Defining function 
 def remove_characters (original_string,n):
     print ("The original string is", original_string)
@@ -8,3 +11,12 @@ def remove_characters (original_string,n):
         result = original_string [n:]
         return result
 
+original_string = "programming"
+
+# Remove first four characters from the original string 
+
+print (remove_characters(original_string, 4))
+
+# Remove first seven characters from the original string 
+
+print (remove_characters(original_string, 7))
