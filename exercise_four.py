436d5670fbf5f0557b8343008789df4213939f21
Angela Ferrer <gelferrero01@gmail.com>
2024-01-25 01:29:37 +0800

    print result

--- exercise_four.py
+++ exercise_four.py
@@ -2,7 +2,7 @@
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
 
 # Display Message 
-print ('Removing certain number of characters from original string...')
+print ('Removing certain number of first characters from original string...')
 
 # Defining function 
 def remove_characters (original_string,n):
@@ -11,12 +11,10 @@ def remove_characters (original_string,n):
         result = original_string [n:]
         return result
 
+# Define original string 
 original_string = "programming"
 
-# Remove first four characters from the original string 
-
-print (remove_characters(original_string, 4))
-
 # Remove first seven characters from the original string 
+print (remove_characters(original_string,7))
 
-print (remove_characters(original_string, 7))
+# Remove first ten characters from the original string 
